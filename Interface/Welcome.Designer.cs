469fea7e46eab1dd2614ddb356941f9931ac7075
Houdaaaa <Houda-11.03@hotmail.com>
2018-03-28 12:24:11 +0200

    update

--- Interface/Welcome.Designer.cs
+++ Interface/Welcome.Designer.cs
@@ -36,9 +36,10 @@
             // Start
             // 
             this.Start.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Start.Location = new System.Drawing.Point(443, 363);
+            this.Start.Location = new System.Drawing.Point(591, 447);
+            this.Start.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Start.Name = "Start";
-            this.Start.Size = new System.Drawing.Size(167, 73);
+            this.Start.Size = new System.Drawing.Size(223, 90);
             this.Start.TabIndex = 0;
             this.Start.Text = "Start";
             this.Start.UseVisualStyleBackColor = true;
@@ -47,24 +48,27 @@
             // Admin
             // 
             this.Admin.Font = new System.Drawing.Font("Cambria", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Admin.Location = new System.Drawing.Point(40, 121);
+            this.Admin.Location = new System.Drawing.Point(53, 149);
+            this.Admin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Admin.Name = "Admin";
-            this.Admin.Size = new System.Drawing.Size(167, 73);
+            this.Admin.Size = new System.Drawing.Size(223, 90);
             this.Admin.TabIndex = 1;
             this.Admin.Text = "Admin";
             this.Admin.UseVisualStyleBackColor = true;
             this.Admin.Click += new System.EventHandler(this.Admin_Click);
             // 
-            // UserControl2
+            // Welcome
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.Controls.Add(this.Admin);
             this.Controls.Add(this.Start);
-            this.Name = "UserControl2";
-            this.Size = new System.Drawing.Size(1052, 592);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Welcome";
+            this.Size = new System.Drawing.Size(1403, 729);
+            this.Load += new System.EventHandler(this.Welcome_Load);
             this.ResumeLayout(false);
 
         }
